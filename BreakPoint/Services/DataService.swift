25f60d715f3f5272a831be913b0b41e7c9d2b54e
Daniel Winship <danielwinship@icloud.com>
2018-02-02 23:34:29 -0500

    Creates function to create DB User in firebase

--- BreakPoint/Services/DataService.swift
+++ BreakPoint/Services/DataService.swift
@@ -37,7 +37,9 @@ class DataService {
         return _REF_FEED
     }
     
-    
+    func createDBUser(uid: String, userData: Dictionary<String, Any>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
     
     
     
