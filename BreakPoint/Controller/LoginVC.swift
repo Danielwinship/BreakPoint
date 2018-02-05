6a8c6590361eb51d41eb873b4772df3fa7fff6d1
Daniel Winship <danielwinship@icloud.com>
2018-02-05 07:06:12 -0500

    Added back dismiss if Auth succeeds

--- BreakPoint/Controller/LoginVC.swift
+++ BreakPoint/Controller/LoginVC.swift
@@ -33,6 +33,7 @@ class LoginVC: UIViewController {
                 AuthService.instance.registerUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, userCreationComplete: { (success, registrationError) in
                     if success {
                         AuthService.instance.loginUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, loginComplete: { (success, nil) in
+                            self.dismiss(animated: true, completion: nil)
                             print("successfully registered user")
                         })
                     } else {
