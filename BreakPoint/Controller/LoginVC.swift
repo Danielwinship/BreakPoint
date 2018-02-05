659c6d8aa62359c73db5107c28f7a73ea10ab714
Daniel Winship <danielwinship@icloud.com>
2018-02-05 06:07:49 -0500

    Connected IBActions and Outlets to LoginVC

--- BreakPoint/Controller/LoginVC.swift
+++ BreakPoint/Controller/LoginVC.swift
@@ -9,27 +9,19 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    
+    @IBOutlet weak var emailField: InsetTextField!
+    
+    @IBOutlet weak var passwordField: InsetTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func signInButtonWasPressed(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
