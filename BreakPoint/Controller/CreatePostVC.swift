5140aea9e9df2dc763db1d6c78b9ac9a480f8542
Daniel Winship <danielwinship@icloud.com>
2018-02-07 06:06:24 -0500

    Built out function on CreatePostVC to send message to Firebase

---
+++ BreakPoint/Controller/CreatePostVC.swift
@@ -0,0 +1,54 @@
+//
+//  CreatePostVC.swift
+//  BreakPoint
+//
+//  Created by Daniel Winship on 2/7/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+       
+    }
+
+    
+    
+    @IBAction func sendButtonWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text == "Say something here..." {
+            sendButton.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, foUID:(Auth.auth().currentUser?.uid)! , withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendButton.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendButton.isEnabled = true
+                    print("There was an error")
+                }
+            })
+        }
+        
+    }
+    @IBAction func closeButtonWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
+
+extension CreatePostVC:UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
--- BreakPoint/Services/DataService.swift
+++ BreakPoint/Services/DataService.swift
@@ -41,7 +41,7 @@ class DataService {
         REF_USERS.child(uid).updateChildValues(userData)
     }
     
-    func uploadPost(withMessage message:String, foUID uid:String, withGroupKey goupKey: String?, sendComplete: @escaping (_ status: Bool) ->()) {
+    func uploadPost(withMessage message:String, foUID uid:String, withGroupKey groupKey: String?, sendComplete: @escaping (_ status: Bool) ->()) {
         if groupKey != nil {
             //send to groups ref
         } else {
