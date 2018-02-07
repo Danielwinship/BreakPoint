21ad467c66b184b0c7354cd91d9ebb62bd4b931d
Daniel Winship <danielwinship@icloud.com>
2018-02-07 05:50:27 -0500

    Created MeVC

---
+++ BreakPoint/Controller/MeVC.swift
@@ -0,0 +1,32 @@
+//
+//  MeVC.swift
+//  BreakPoint
+//
+//  Created by Daniel Winship on 2/6/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signOutButtonWasPressed(_ sender: Any) {
+        
+    }
+    
+
+}
