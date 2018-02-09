d32688cd63f0c206fd31e816dbc698524a319a1a
Daniel Winship <danielwinship@icloud.com>
2018-02-09 05:42:19 -0500

    Built out UserCell on CreateGroupsVC

---
+++ BreakPoint/View/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  BreakPoint
+//
+//  Created by Daniel Winship on 2/9/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
