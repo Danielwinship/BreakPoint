db018e9b08c878dea44e31ffdc330fc4a1f3e89d
Daniel Winship <danielwinship@icloud.com>
2018-02-08 21:21:55 -0500

    Cleaned up formatting

--- BreakPoint/Controller/CreateGroupsVC.swift
+++ BreakPoint/Controller/CreateGroupsVC.swift
@@ -10,30 +10,30 @@ import UIKit
 
 class CreateGroupsVC: UIViewController {
     
+    //Outlets
     @IBOutlet weak var titleTextField: InsetTextField!
     @IBOutlet weak var descriptionTextField: InsetTextField!
     @IBOutlet weak var emailSearchTextField: InsetTextField!
     @IBOutlet weak var groupMemberLabel: UILabel!
     @IBOutlet weak var doneButton: UIButton!
-    
-    
     @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
     
     
     @IBAction func doneButtonWasPressed(_ sender: Any) {
+        
     }
     
     @IBAction func closeButtonWasPressed(_ sender: Any) {
+        
     }
     
     
