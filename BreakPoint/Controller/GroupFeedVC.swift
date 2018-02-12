b79448b4096e28170eec411d060f03f6427033fa
Daniel Winship <danielwinship@icloud.com>
2018-02-12 06:43:02 -0500

    Completing final config of GroupFeedVC

--- BreakPoint/Controller/GroupFeedVC.swift
+++ BreakPoint/Controller/GroupFeedVC.swift
@@ -10,12 +10,28 @@ import UIKit
 
 class GroupFeedVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupTitleLabel: UILabel!
+    @IBOutlet weak var membersLabel: UILabel!
+    @IBOutlet weak var messageTextField: InsetTextField!
+    
+    @IBOutlet weak var sendBtn: UIButton!
+    @IBOutlet weak var sendBtnView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+       
     }
 
-   
-
+    @IBAction func backButtonWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendButtonWasPressed(_ sender: Any) {
+    }
+    
 }
