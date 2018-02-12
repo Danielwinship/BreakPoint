533ea1f6f1a1ef8e9fdaeb53ebc2cf70f60c8cb1
Daniel Winship <danielwinship@icloud.com>
2018-02-12 06:19:40 -0500

    Built out GroupFeedCell and GroupFeedVC

---
+++ BreakPoint/Controller/GroupFeedVC.swift
@@ -0,0 +1,21 @@
+//
+//  GroupFeedVC.swift
+//  BreakPoint
+//
+//  Created by Daniel Winship on 2/12/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+}
---
+++ BreakPoint/View/GroupFeedCell.swift
@@ -0,0 +1,27 @@
+//
+//  GroupFeedCell.swift
+//  BreakPoint
+//
+//  Created by Daniel Winship on 2/12/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content:String) {
+        self.profileImage.image = profileImage
+        self.emailLabel.text = email
+        self.contentLabel.text = content
+        
+    }
+    
+}
